a3535ca812e6e2ef9063dbb6bdde619fe5a46e57
mahe <benoitmahe5@gmail.com>
2022-11-22 14:33:13 +0100

    seeds phase 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,15 +18,15 @@ puts "...finished !"
 
 puts "creation user owner..."
 user_owner1 = User.create!(name: 'Daniel', email: 'daniel@gmail.com', password: 'jackdaniel')
-user_owner2 = User.create!(name: 'Daniel', email: 'daniel@gmail.com', password: 'jackdaniel')
+user_owner2 = User.create!(name: 'Jean', email: 'jean@gmail.com', password: 'jeanjack')
 puts "...finished !"
 
 puts "creation spaceship..."
-spaceship1 = Spaceship.create!(name: 'Black Pearl', price: 20_000.40, address: "5 rue de l'Atlantique", seats: 15, energy: 'diesel', gravity: true, user: user_owner)
-spaceship2 = Spaceship.create!(name: 'Nimbus 2000', price: 2_100.90, address: "10 rue du Ciel", seats: 2, energy: 'diesel', gravity: true, user: user_owner)
-spaceship3 = Spaceship.create!(name: 'Black Pearl', price: 3_000.50, address: "5 rue de l'Atlantique", seats: 15, energy: 'diesel', gravity: true, user: user_owner)
-spaceship4 = Spaceship.create!(name: 'Le Wagon', price: 3_000.50, address: "10 passage de la poule noire", seats: 27, energy: 'diesel', gravity: true, user: user_owner)
-spaceship5 = Spaceship.create!(name: 'Faucon millenium', price: 65_000, address: "7 avenue de l'Espace", seats: 5, energy: 'diesel', gravity: true, user: user_owner)
+spaceship1 = Spaceship.create!(name: 'Black Pearl', price: 20_000.40, address: "5 rue de l'Atlantique", seats: 15, energy: 'diesel', gravity: true, user: user_owner1)
+spaceship2 = Spaceship.create!(name: 'Nimbus 2000', price: 2_100.90, address: "10 rue du Ciel", seats: 2, energy: 'diesel', gravity: true, user: user_owner2
+spaceship3 = Spaceship.create!(name: 'Black Pearl', price: 3_000.50, address: "5 rue de l'Atlantique", seats: 15, energy: 'diesel', gravity: true, user: user_owner1)
+spaceship4 = Spaceship.create!(name: 'Le Wagon', price: 3_000.50, address: "10 passage de la poule noire", seats: 27, energy: 'diesel', gravity: true, user: user_owner1)
+spaceship5 = Spaceship.create!(name: 'Faucon millenium', price: 65_000, address: "7 avenue de l'Espace", seats: 5, energy: 'diesel', gravity: true, user: user_owner2)
 puts "...finished !"
 
 puts "creation booking..."
