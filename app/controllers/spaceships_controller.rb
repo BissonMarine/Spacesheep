92b59454d58ef9096ff3e1652835698b0d988930
mahe <benoitmahe5@gmail.com>
2022-11-23 11:04:16 +0100

    add show step 1

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,2 +1,15 @@
 class SpaceshipsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
+
+  def show
+    @spaceship = Spaceship.find(params[:id])
+    @spaceship.name
+    @spaceship.price
+    @spaceship.description
+    @spaceship.weapons
+    @spaceship.energy
+    @spaceship.seats
+    @spaceship.address
+    @spaceship.gravity
+  end
 end
