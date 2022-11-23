7bf9f60e6cba8a22e1f2916c8628f1a888b55567
Marine Bisson <marinemaute@free.fr>
2022-11-23 15:46:48 +0100

    Create booking ctrller methods to create, update, destroy, etc. bookings -> Sign in MANDATORY for next step

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,42 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(task_params)
+    @booking.save
+    redirect_to spaceships_path
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    @booking.save
+    # redirect_to bookings_path
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:total_price, :booking_start_date, :booking_end_date)
+  end
 end
