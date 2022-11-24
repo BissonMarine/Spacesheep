e7f2894a2a3ddd2273f58a6fc0fd56fcb1a00695
mahe <benoitmahe5@gmail.com>
2022-11-24 14:21:15 +0100

    add login form - new and create booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def new
+    @spaceship = Spaceship.find(params[:id])
+    @booking = Booking.new
+  end
+
+  def create
+    @spaceship = Spaceship.find(params[:id])
+    @booking = Booking.new(booking_params)
+    @booking.spaceship = @spaceship
+
+    if @booking.save
+      redirect_to bookings_path(@bookings)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:booking_start_date, :booking_end_date)
+  end
 end
