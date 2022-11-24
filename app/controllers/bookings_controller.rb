b03f85856ad5426d0d5e2fb4f5a84c458f0101e4
Jéeremy Martinet <jeremartinet@gmail.com>
2022-11-24 14:20:56 +0100

    v1 bookings index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @user_bookings = Booking.where(user: User.find(19))
+    # identifier le user actuel
+    # regarder si il y a des bookings liés à son spaceship
+    # les afficher
+
+    # @user = User.find(params(User.name == "jean"))
+    @user_spaceships = Spaceship.where(user: current_user) #retourne un array contenant les obj spships associés au user
+
+    @owner_bookings = Booking.includes(:spaceship).where(spaceship: { user: current_user })
+  end
 end
