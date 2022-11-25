8ccd995d4e76b7951d753e2deca143cdf932060b
Jéeremy Martinet <jeremartinet@gmail.com>
2022-11-25 13:05:53 +0100

    valid or decline

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,11 +4,13 @@
 
 import { application } from "./application"
 
+import AcceptDeclineBookingController from "./accept_decline_booking_controller"
+application.register("accept-decline-booking", AcceptDeclineBookingController)
+
 import AnimatedStarsJsController from "./animated_stars_js_controller"
 application.register("animated-stars-js", AnimatedStarsJsController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import TypedJsController from "./typed_js_controller"
-application.register("typed-js", TypedJsController)
+
