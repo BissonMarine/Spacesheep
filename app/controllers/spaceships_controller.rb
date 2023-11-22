3e54fabf84f980b870e800810b6db74071c35aff
Marine Bisson <marinemaute@free.fr>
2023-11-22 11:02:21 +0100

    test

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,5 +1,5 @@
 class SpaceshipsController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def show
     @spaceship = Spaceship.find(params[:id])
